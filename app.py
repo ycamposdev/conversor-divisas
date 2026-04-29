b8125ea861f069d7c562d58d96f3f47c0b92edb5
ycamposdev <ytalo.llamoccacampos@outlook.es>
2026-04-29 01:05:20 -0500

    feat: datos en comboboxs y calculo

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
+import requests
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
+API_Exchange="https://v6.exchangerate-api.com/v6/b173cea5e2936d72157593c8/latest/USD"
 
-@app.route('/')
-def hola_mundo():
-    return '<h1>Bienvenido Flask</h1><p>El servidor esta en ejecución.</p>'
+def getMoneda():
+    try:
+      response = requests.get(API_Exchange)
+      return response.json()
+    except:
+     return None
 
-@app.route("/categorias")
-def categoria():
-    return "<h1>Estas son las categorias</h1>"
+def handleConvertir():
+   cantidad=float(request.form.get('cantidad'))
+   monedaOrigen=request.form.get("moneda_origen") #PEN
+   monedaDestino=request.form.get("moneda_destino")
+   resultado = cantidad / getMoneda()["conversion_rates"][monedaOrigen] * getMoneda()["conversion_rates"][monedaDestino]
+   return resultado
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    datos=getMoneda()
+    rest=None
+    if(request.method=='POST'):
+       resultado=handleConvertir()
+       rest=resultado
+    return render_template('index.html', data=datos, result=rest)
 
 # Definir el PORT de escucha
 if __name__ == '__main__':
-   
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5050)
